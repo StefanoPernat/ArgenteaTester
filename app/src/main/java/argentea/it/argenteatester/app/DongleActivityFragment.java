4a635eca53ddd920b52ae5c8de7f5613a4c8b7a4
Stefano Pernat <stefano.pernat@gmail.com>
2015-10-24 19:04:37 +0200

    First commit

---
+++ app/src/main/java/argentea/it/argenteatester/app/DongleActivityFragment.java
@@ -0,0 +1,22 @@
+package argentea.it.argenteatester.app;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DongleActivityFragment extends Fragment {
+
+    public DongleActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dongle, container, false);
+    }
+}
